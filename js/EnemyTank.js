a11cd1743fd7cbd8eed352b1922aa5039ddfbd7b
mxhdsn <92098916+mxhdsn@users.noreply.github.com>
2021-12-13 16:13:16 +0000

    Monday Afternoon
    Bullet Creation
    Enemy Movement
    Player Tracking (Turret Rotation)

--- js/EnemyTank.js
+++ js/EnemyTank.js
@@ -2,6 +2,8 @@ class EnemyTank extends BaseTank {
 
     /** @type {PlayerTank} */
     player
+    /** @type {number} */
+    tankSpeed = 100
 
     constructor(scene, x, y, texture, frame, player){
         super(scene, x, y, texture, frame)
@@ -10,10 +12,11 @@ class EnemyTank extends BaseTank {
     }
 
     initMovement(){
-        this.scene.physics.velocityFromRotation(this.hull.rotation, this.tankSpeed,this.hull.body.rotation)
+        this.scene.physics.velocityFromRotation(this.hull.rotation, this.tankSpeed,this.hull.body.velocity)
     }
 
     update(time, delta){
         super.update()
+        this.turret.rotation = Phaser.Math.Angle.Between(this.hull.x, this.hull.y, this.player.hull.x, this.player.hull.y)
     }
 }
--- js/TankScene.js
+++ js/TankScene.js
@@ -6,9 +6,16 @@ class TankScene extends Phaser.Scene {
     destructLayer
     /** @type {PlayerTank} */
     player
+    /** @type {Array.<EnemyTank>} */
+    enemyTanks = []
+    /** @type {Phaser.Physics.Arcade.Group} */
+    bullets
+    /** @type {Phaser.Physics.Arcade.Group} */
+    enemyBullets
 
     preload() {
         //-- Preload Assets --//
+        this.load.image('bullet', 'assets/tanks/bullet.png')
         this.load.atlas('tank', 'assets/tanks/tanks.png', 'assets/tanks/tanks.json')
         this.load.atlas('enemy', 'assets/tanks/enemy-tanks.png', 'assets/tanks/tanks.json')
         this.load.image('tileset', 'assets/tanks/landscape-tileset.png')
@@ -24,6 +31,15 @@ class TankScene extends Phaser.Scene {
         this.destructLayer.setCollisionByProperty({collides: true})
         this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
         this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
+        //-- Create Bullets --//
+        this.enemyBullets = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 10
+        })
+        this.bullets = this.physics.add.group({
+            defaultKey: 'bullet',
+            maxSize: 5
+        })
         const objectLayer = this.map.getObjectLayer('objectLayer')
         let enemyObjects = []
         let actor
@@ -44,10 +60,22 @@ class TankScene extends Phaser.Scene {
 
     update(time, delta) {
         this.player.update()
+        for(let i = 0; i < this.enemyTanks.length; i++){
+            this.enemyTanks[i].update(time, delta)
+        }
     }
 
     createEnemy(dataObject){
         let enemyTank = new EnemyTank(this, dataObject.x, dataObject.y, 'enemy', 'tank1', this.player)
+        enemyTank.initMovement()
+        enemyTank.enableCollision(this.destructLayer)
+        this.physics.add.collider(enemyTank.hull, this.player.hull)
+        this.enemyTanks.push(enemyTank)
+        if(this.enemyTanks.length > 1){
+            for(let i = 0; i < this.enemyTanks.length - 1; i++){
+                this.physics.add.collider(enemyTank.hull, this.enemyTanks[i].hull)
+            }
+        }
     }
 
     createPlayer(dataObject) {
