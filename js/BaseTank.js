136739e3a7d193b0485734848a9c004b4ea627d8
mxhdsn <92098916+mxhdsn@users.noreply.github.com>
2021-12-13 12:31:19 +0000

    Monday Morning Progress
    Tilemaps Added

---
+++ js/BaseTank.js
@@ -0,0 +1,39 @@
+class BaseTank {
+
+    /** @type {number} */
+    tankSpeed = 100
+    /** @type {number} */
+    damageMax = 2
+
+    constructor(scene, x, y, texture, frame){
+        
+    }
+
+    update(){
+
+    }
+
+    damage(){
+
+    }
+
+    setBullets(){
+
+    }
+
+    burn(){
+
+    }
+
+    isDestroyed(){
+
+    }
+
+    isImmobilised(){
+
+    }
+
+    enableCollision(){
+
+    }
+}
--- js/TankScene.js
+++ js/TankScene.js
@@ -1,10 +1,29 @@
 class TankScene extends Phaser.Scene {
+
+    /** @type {Phaser.Tilemaps.Tilemap} */
+    map
+    /** @type {Phaser.Tilemaps.TilemapLayer} */
+    destructLayer
+
     preload() {
-
+        //-- Preload Assets --//
+        this.load.image('tileset', 'assets/tanks/landscape-tileset.png')
+        this.load.tilemapTiledJSON('level1', 'assets/level1.json')
     }
+
     create() {
+        //-- Load in Tilemap --//
+        this.map = this.make.tilemap({key:'level1'})
+        const landscape = this.map.addTilesetImage('landscape-tileset', 'tileset')
+        this.map.createLayer('groundLayer', [landscape], 0, 0)
+        this.destructLayer = this.map.createLayer('destructibleLayer', [landscape], 0, 0)
+        this.destructLayer.setCollisionByProperty({collides: true})
+        this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
+        this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels)
 
+        //-- Player Tank --//
     }
+
     update(time, delta) {
 
     }
